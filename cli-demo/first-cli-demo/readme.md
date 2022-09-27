feb039605d65e46ebd7cf27938646fc9ba68e9dd
gonglei <291523450@qq.com>
2022-09-27 16:38:36 +0800

    更新组件

---
+++ cli-demo/first-cli-demo/readme.md
@@ -0,0 +1,32 @@
+- npm init 初始一个项目
+- 在项目中新建 bin 文件夹
+- 在项目 bin 文件夹下新建一个 first-cli.js
+- 在 first-cli.js 文件中编写代码
+
+```js
+#!/usr/bin/env node
+
+function run(argv) {
+  console.log("argv", argv);
+  if (argv[0] === "-v" || argv[0] === "--version") {
+    console.log("  version is 0.0.1");
+  } else if (argv[0] === "-h" || argv[0] === "--help") {
+    console.log("  usage:\n");
+    console.log("  -v --version [show version]");
+  }
+}
+run(process.argv.slice(2));
+```
+
+- 在 package.json 中引入编写的文件
+
+```json
+  "bin": {
+    "first-cli": "bin/first-cli.js"
+  },
+```
+
+- 在项目中 npm link 弄一个本地镜像包
+
+- 在要用到脚手架的项目中 npm link first-cli
+- 然后输入 first-cli -v 指令中打印出脚手架对应的版本
--- lg-ui-vue/rollup.config.js
+++ lg-ui-vue/rollup.config.js
@@ -1,19 +1,17 @@
-import vue from 'rollup-plugin-vue' //
+import vue from 'rollup-plugin-vue'; //
 import commonjs from 'rollup-plugin-commonjs';
-import {
-    terser
-} from 'rollup-plugin-terser';
-
-
+import { terser } from 'rollup-plugin-terser';
+import image from 'rollup-plugin-image';
 export default {
-    input: "./src/index.js",
-    output: {
-        file: "./dist/index.cjs.js",
-        format: "cjs",
-    },
-    plugins: [
-        vue(),
-        commonjs(),
-        terser(), //可以根据情况来处理打包后文件压不压缩
-    ],
-}
+  input: './src/index.js',
+  output: {
+    file: './dist/index.cjs.js',
+    format: 'cjs'
+  },
+  plugins: [
+    vue(),
+    commonjs(),
+    // terser(), //可以根据情况来处理打包后文件压不压缩
+    image()
+  ]
+};
